5d011c95ce010d197b5014613cacd7b20ffab337
nikocudder <81013645+nikocudder@users.noreply.github.com>
2021-05-26 20:38:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function equal()
+{
+    d=document
+    var a = Number(d.form1.A.value);
+    var b = Number(d.form1.B.value);
+    var q;
+    if (a===b)
+    {
+        q="Числа равны";
+    }
+    else {q="Числа не равны";}
+    d.form1.q.value=q;
+}
+
+
+function Show()
+{
+    d=document
+    var admin, name;
+    name = "Nikita";
+    admin = name;
+    alert(admin);
+}
+
+function Age()
+{
+    d=document
+    do {age = +prompt("Укажите ваш возраст");}
+    while 
+    (confirm("Вы уверены, что вам " +age)===false)
+    alert("Вам " +age);
+}
+
+function Com()
+{
+    d=document
+    let ok = confirm('Отправить данные на сервер')
+    if (ok) {
+        alert('Данные успешно отправлены на сервер');
+    } else {
+        alert('Отправка на сервер прервана!');
+    }
+}
